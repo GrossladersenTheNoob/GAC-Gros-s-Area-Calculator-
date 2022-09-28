fdafca142a23706c879ff278719b7f0b4db372ac
Großladersen <113502256+GrossladersenTheNoob@users.noreply.github.com>
2022-09-28 13:32:23 +0700

    spl.py
    Added spalshes
    English

---
+++ spl.py
@@ -0,0 +1,12 @@
+import random
+import time
+import sys
+import datetime
+from calVie import *
+from cred import *
+from cal import *
+from qNcon import *
+def spl():
+    list1 = ['Ài ố sì mà', 'GAC > Calculator fr', "Grosladersen's the worst coder ever", 'Me when', 'Imagine you was force to install this stoopid program', "It's by birthday!"]
+    print(random.choice(list1))
+spl()
